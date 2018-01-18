03f7cc6e7e293471e09b79c68dc18da8089c0bcc
Yuexiz <yuexi.z@gmail.com>
2018-01-18 17:06:33 +0800

    创建Pod

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '8.0'
+use_frameworks!
+inhibit_all_warnings!
+
+target 'RAC-Demo' do
+    
+    pod 'ReactiveCocoa', :git => 'https://github.com/zhao0/ReactiveCocoa.git', :tag => '2.5.2'
+
+end
---
+++ RAC-Demo/MainViewController.h
@@ -0,0 +1,14 @@
+//
+//  MainViewController.h
+//  RAC-Demo
+//
+//  Created by Yuexiz on 2018/1/18.
+//  Copyright © 2018年 Yuexiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+
+@interface MainViewController : BaseViewController
+
+@end
